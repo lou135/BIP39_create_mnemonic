4168c868b21498e16eac6568d6bd45131efad44a
lou135 <122233597+lou135@users.noreply.github.com>
2024-08-17 21:00:18 +0100

    Update BIP39_mnemonic.py

--- BIP39_mnemonic.py
+++ BIP39_mnemonic.py
@@ -71,7 +71,7 @@ def bit128_to_hash(n):
 def hash_to_bit(n):
     valor=n[0]
     bits=['0000','0001','0010','0011','0100','0101','0110','0111','1000','1001','1010','1011','1100','1101','1110','1111']
-    letras=['a','b','c','d','f']
+    letras=['a','b','c','d','e','f']
     for i,h in enumerate(letras):
         if valor==h:
             valor=10+i
